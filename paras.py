26a7f6d1d1c69c160c1e603a76c8260a3400abd9
ChouguleParas07 <paraschougule1008@gmail.com>
2025-09-22 16:17:21 +0530

    just for practice

--- paras.py
+++ paras.py
@@ -2,3 +2,8 @@ n = int(input("enter any number - "))
 n = abs(n)
 p = ["EVEN", "ODD"]
 print("then given number is ",p[n%2])
+# 2. positive or negative
+if n > 0:
+    print("the given number is positive")   
+elif n < 0:
+    print("the given number is negative")
